0fcef9ec7e92bf1474fe6bde06b7a94afbb11c19
rbrtj <robertjaszczurek@icloud.com>
2022-07-07 08:32:09 +0200

    Generated new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+import { BackupTasksComponent } from './backup-tasks/backup-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    BackupTasksComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/backup-tasks/backup-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-backup-tasks',
+  templateUrl: './backup-tasks.component.html',
+  styleUrls: ['./backup-tasks.component.scss']
+})
+export class BackupTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
